ef0d403ab99645a76e6d83121e8ce8a1f07a4874
sravani2604 <44571705+sravani2604@users.noreply.github.com>
2018-10-30 16:59:48 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Test_task
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Please select the Player Aution System");
+			Console.WriteLine("Add player name");
+			Console.WriteLine("Display name as Sravani ");
+			Console.WriteLine("Exit");
+			Console.ReadLine();
+
+		}
+	}
+}
